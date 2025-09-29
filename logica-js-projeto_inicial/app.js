7754e5eed79532c397c482a9b6abbebca8dcfc60
NatashaB-randao <natashabrandao57@hotmail.com>
2025-09-29 14:06:51 -0300

    Projeto

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,8 @@
+alert('Boas vindas ao jogo do número secreto!');
+
+let numeroSecreto = 29;
+let chute = prompt('Escolha um número entre 0 e 30:');
+
+if (chute == numeroSecreto) {
+    console.log('Parabéns! Você acertou o número secreto!');
+}
