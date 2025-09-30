95e1be51cef3d9a61f5cfbc95b10cb2f7c78a3f6
NatashaB-randao <natashabrandao57@hotmail.com>
2025-09-30 11:40:08 -0300

    Else e Comentários

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,8 +1,14 @@
 alert('Boas vindas ao jogo do número secreto!');
 
 let numeroSecreto = 29;
+console.log(numeroSecreto);
+
+// Solicitar ao usuário que faça um chute
 let chute = prompt('Escolha um número entre 0 e 30:');
 
+// Se o chute for igual ao número secreto, exiba uma mensagem de parabéns
 if (chute == numeroSecreto) {
-    console.log('Parabéns! Você acertou o número secreto!');
+    alert('Parabéns! Você acertou o número secreto!');
+} else {
+    alert('Que pena! Você errou.');
 }
