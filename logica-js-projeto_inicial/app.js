a7f97bd62af48a0961d7f3cacd3f922d7caf16af
NatashaB-randao <natashabrandao57@hotmail.com>
2025-10-01 12:09:47 -0300

    Novas condições, While

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -3,12 +3,20 @@ alert('Boas vindas ao jogo do número secreto!');
 let numeroSecreto = 29;
 console.log(numeroSecreto);
 
-// Solicitar ao usuário que faça um chute
-let chute = prompt('Escolha um número entre 0 e 30:');
+let chute;
 
-// Se o chute for igual ao número secreto, exiba uma mensagem de parabéns
-if (chute == numeroSecreto) {
-    alert(`Parabéns! Você acertou o número secreto! O número era ${numeroSecreto}.`);
-} else {
-    alert('Que pena! Você errou.');
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 0 e 30:');
+
+    // Se o chute for igual ao número secreto, exiba uma mensagem de parabéns
+    if (chute == numeroSecreto) {
+        alert(`Parabéns! Você acertou o número secreto! O número era ${numeroSecreto}.`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}. Tente novamente!`);
+        } else {
+            alert(`O número secreto é maior que ${chute}. Tente novamente!`);
+        }
+    }
 }
+
