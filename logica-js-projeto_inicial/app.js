d53b605b004aab2b22d11be97ce79f4d6edee454
NatashaB-randao <natashabrandao57@hotmail.com>
2025-10-01 12:16:21 -0300

    Tentativas

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -4,19 +4,21 @@ let numeroSecreto = 29;
 console.log(numeroSecreto);
 
 let chute;
+let tentativas = 1;
 
 while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 0 e 30:');
 
     // Se o chute for igual ao número secreto, exiba uma mensagem de parabéns
     if (chute == numeroSecreto) {
-        alert(`Parabéns! Você acertou o número secreto! O número era ${numeroSecreto}.`);
+        alert(`Parabéns! Você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
     } else {
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}. Tente novamente!`);
         } else {
             alert(`O número secreto é maior que ${chute}. Tente novamente!`);
         }
+        tentativas++;
     }
 }
 
