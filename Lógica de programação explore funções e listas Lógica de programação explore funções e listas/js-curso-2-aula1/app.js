59b6ed0e569de11bf52f57e96d8270f4dda4c674
NatashaB-randao <natashabrandao57@hotmail.com>
2025-10-02 13:35:06 -0300

    Manipulando textos

---
+++ Lógica de programação explore funções e listas Lógica de programação explore funções e listas/js-curso-2-aula1/app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do número secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Digite um número entre 0 e 10:';
