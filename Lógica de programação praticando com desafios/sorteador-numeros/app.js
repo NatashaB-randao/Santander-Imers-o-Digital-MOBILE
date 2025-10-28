837850ab5ffb60937e2c47b06caf49449b9068e6
NatashaB-randao <natashabrandao57@hotmail.com>
2025-10-28 13:08:32 -0300

    criando função para sortear

--- Lógica de programação praticando com desafios/sorteador-numeros/app.js
+++ Lógica de programação praticando com desafios/sorteador-numeros/app.js
@@ -1,7 +1,20 @@
 function sortear(){
     let quantidade = document.getElementById("quantidade").value;
-    let de = document.getElementById("de").value;
+    let de = parseInt(document.getElementById("de").value);
     let ate = document.getElementById("ate").value;
 
-    alert(`Quantidade: ${quantidade}\nDe: ${de}\nAté: ${ate}`);
+    let sorteados = [];
+    let numero;
+
+    for(let i = 0; i < quantidade; i++) {
+        numero = obterNumeroAleatorio(de, ate);
+        sorteados.push(numero);
+    }
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = <label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>
+}
+
+function obterNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
