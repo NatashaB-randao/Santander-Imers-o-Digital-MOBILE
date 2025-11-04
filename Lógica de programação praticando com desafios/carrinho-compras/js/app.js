616834c94fa863df336710dc91283d5ffc43bb6e
NatashaB-randao <natashabrandao57@hotmail.com>
2025-11-04 13:07:55 -0300

    Calculando o valor do produto

--- Lógica de programação praticando com desafios/carrinho-compras/js/app.js
+++ Lógica de programação praticando com desafios/carrinho-compras/js/app.js
@@ -1,10 +1,15 @@
 function adicionar() {
     // recupera os valores dos campos - nome do produto, quantidade e valor
-    let nomeProduto = document.getElementById("produto");
+    let produto = document.getElementById("produto").value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
     let quantidade = parseInt(document.getElementById("quantidade"));
     let valor = parseFloat(document.getElementById("valor").value);
 
     // cakcular o preço, o nosso subtotal
+    let preco = quantidade.value * valorUnitario;
+
+
     // adicionar produto no carrinho de compras (tabela)
     // atualizar o valor total da compra
 
