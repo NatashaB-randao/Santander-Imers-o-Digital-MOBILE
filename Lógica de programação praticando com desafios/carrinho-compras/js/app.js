dccb4f44a4123059a21ef8979ada18d5de877bce
NatashaB-randao <natashabrandao57@hotmail.com>
2025-11-04 12:27:07 -0300

    Obtendo o produto adicionado

---
+++ Lógica de programação praticando com desafios/carrinho-compras/js/app.js
@@ -0,0 +1,16 @@
+function adicionar() {
+    // recupera os valores dos campos - nome do produto, quantidade e valor
+    let nomeProduto = document.getElementById("produto");
+    let quantidade = parseInt(document.getElementById("quantidade"));
+    let valor = parseFloat(document.getElementById("valor").value);
+
+    // cakcular o preço, o nosso subtotal
+    // adicionar produto no carrinho de compras (tabela)
+    // atualizar o valor total da compra
+
+    
+}
+
+function limpar() {
+
+}
