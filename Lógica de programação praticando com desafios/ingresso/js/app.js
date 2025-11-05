49ebf31012f6edfabceac47935ea3b38368d2fd7
NatashaB-randao <natashabrandao57@hotmail.com>
2025-11-05 13:16:35 -0300

    Realizando compra para tipo pista

--- Lógica de programação praticando com desafios/ingresso/js/app.js
+++ Lógica de programação praticando com desafios/ingresso/js/app.js
@@ -1,8 +1,19 @@
 function comprar() {
     let tipo = document.getElementById('tipo-ingresso').value;
-    let qtd = document.getElementById('qtd').value;
+    let qtd = parseInt(document.getElementById('qtd').value);
 
-    if (tipo.value == 'Pista') {
-        comprarPista(qtd.value);
+    if (tipo.value == 'pista') {
+        comprarPista(qtd);
+    }
+}
+
+function comprarPista(qtd) {
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);   
+    if (qtd > qtdPista) {
+        alert('Quantidade indisponível para tipo Pista!');
+    } else {
+        qtdPista = qtdPista - qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso!');
     }
 }
