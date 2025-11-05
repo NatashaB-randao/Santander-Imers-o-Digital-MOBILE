3a473004b86640cbae19fb795f7ccad87bc038c4
NatashaB-randao <natashabrandao57@hotmail.com>
2025-11-05 13:30:26 -0300

    Realizando compra para outros tipos

--- Lógica de programação praticando com desafios/ingresso/js/app.js
+++ Lógica de programação praticando com desafios/ingresso/js/app.js
@@ -2,8 +2,12 @@ function comprar() {
     let tipo = document.getElementById('tipo-ingresso').value;
     let qtd = parseInt(document.getElementById('qtd').value);
 
-    if (tipo.value == 'pista') {
+    if (tipo == 'pista') {
         comprarPista(qtd);
+    } else if (tipo.value == 'superior') {
+        comprarSuperior(qtd);
+    } else {
+        comprarInferior(qtd);
     }
 }
 
@@ -14,6 +18,28 @@ function comprarPista(qtd) {
     } else {
         qtdPista = qtdPista - qtd;
         document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso!');
+    }
+}
+
+function comprarSuperior(qtd) {
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);   
+    if (qtd > qtdSuperior) {
+        alert('Quantidade indisponível para tipo Superior!');
+    } else {
+        qtdSuperior = qtdSuperior - qtd;
+        document.getElementById('qtd-superior').textContent = qtdSuperior;
+        alert('Compra realizada com sucesso!');
+    }
+}
+
+function comprarInferior(qtd) {
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);   
+    if (qtd > qtdInferior) {
+        alert('Quantidade indisponível para tipo Inferior!');
+    } else {
+        qtdInferior = qtdInferior - qtd;
+        document.getElementById('qtd-inferior').textContent = qtdInferior;
         alert('Compra realizada com sucesso!');
     }
 }
