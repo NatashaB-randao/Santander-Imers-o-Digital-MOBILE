5a75fab22f37e447416c3f9ea95a2918718846f0
NatashaB-randao <natashabrandao57@hotmail.com>
2025-11-06 13:21:55 -0300

    Embaralhando o array de amigos

--- Lógica de programação praticando com desafios/amigo-secreto/js/app.js
+++ Lógica de programação praticando com desafios/amigo-secreto/js/app.js
@@ -1,6 +1,9 @@
+let amigos = [];
+
 function adicionar() {
     let amigo = document.getElementById("nome-amigo");
     let lista = document.getElementById("lista-amigos");
+    amigos.push(amigo.value);
 
     if (lista.textContent == '') {
         lista.textContent = amigo.value;
@@ -8,5 +11,20 @@ function adicionar() {
         lista.textContent = lista.textContent + ', ' + amigo.value;
     }
     amigo.value = '';
-    
+}
+
+function sortear() {
+    embaralha(amigos);
+}
+
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
 }
