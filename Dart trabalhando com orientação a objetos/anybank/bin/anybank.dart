4743af93963fc899869413fe349f8d2dddd3b617
NatashaB-randao <natashabrandao57@hotmail.com>
2025-11-24 11:45:59 -0300

    Criando objetos, acessando valores e guardando contas

--- Dart trabalhando com orientação a objetos/anybank/bin/anybank.dart
+++ Dart trabalhando com orientação a objetos/anybank/bin/anybank.dart
@@ -1,9 +1,15 @@
 void main() {
-  List<String> contas = <String>["Sergio", "João" ];
-  List<double> saldos = <double>[1000.0, 2000.0];
-
   Conta contaSergio = Conta("Sergio", 1000.0);
   Conta contaJoao = Conta("João", 2000.0);
+
+  List<Conta> contas = [contaSergio, contaJoao];
+
+  for (var conta in contas) {
+    print("Titular: ${conta.titular}, Saldo: ${conta.saldo}");
+  }
+
+  contaJoao.saldo = 5000.0;
+  print(contaJoao.saldo);
 }
 
 class Conta {
