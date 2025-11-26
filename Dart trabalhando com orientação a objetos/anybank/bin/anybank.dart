895d267082dde34b55950fc329fc14e6b6c43540
NatashaB-randao <natashabrandao57@hotmail.com>
2025-11-26 12:23:01 -0300

    Reaproveitando código com herança

--- Dart trabalhando com orientação a objetos/anybank/bin/anybank.dart
+++ Dart trabalhando com orientação a objetos/anybank/bin/anybank.dart
@@ -3,8 +3,10 @@ import 'package:anybank/conta.dart';
 void main() {
   Conta contaSergio = Conta("Sergio", 1000.0);
   Conta contaJoao = Conta("João", 2000.0);
+  ContaCorrente contaChris = ContaCorrente("Chris", 4000.0);
+  ContaPoupanca contaAna = ContaPoupanca("Ana", 5000.0);
 
-  List<Conta> contas = [contaSergio, contaJoao];
+  List<Conta> contas = [contaSergio, contaJoao, contaChris, contaAna];
 
   for (var conta in contas) {
     conta.imprimeSaldo();
@@ -13,7 +15,11 @@ void main() {
   contaJoao.receber(3000);
   contaSergio.receber(1000);
   contaSergio.enviar(200);
+
+  contaChris.imprimeSaldo();
+  contaChris.receber(1000);
+
+  contaAna.imprimeSaldo();  
+  contaAna.enviar(300);
 }
-
-
 
