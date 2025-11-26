3301508b0af7a0515a8461b105ea031aeb50b829
NatashaB-randao <natashabrandao57@hotmail.com>
2025-11-26 14:27:05 -0300

    Produzindo um molde com classe abstrata

--- Dart trabalhando com orientação a objetos/anybank/bin/anybank.dart
+++ Dart trabalhando com orientação a objetos/anybank/bin/anybank.dart
@@ -1,20 +1,9 @@
 import 'package:anybank/conta.dart';
 
 void main() {
-  Conta contaSergio = Conta("Sergio", 1000.0);
-  Conta contaJoao = Conta("João", 2000.0);
   ContaCorrente contaChris = ContaCorrente("Chris", 4000.0);
   ContaPoupanca contaAna = ContaPoupanca("Ana", 4000.0);
 
-  List<Conta> contas = [contaSergio, contaJoao, contaChris, contaAna];
-
-  for (var conta in contas) {
-    conta.imprimeSaldo();
-  }
-
-  contaJoao.receber(3000);
-  contaSergio.receber(1000);
-  contaSergio.enviar(200);
 
   contaChris.imprimeSaldo();
   contaChris.enviar(4300);
