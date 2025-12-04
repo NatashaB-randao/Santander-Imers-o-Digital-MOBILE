d184d74dbdbd14f4fa8157d80d840d3a8e86bb00
NatashaB-randao <natashabrandao57@hotmail.com>
2025-12-04 13:04:56 -0300

    Fazendo primeiro assincronismo

--- Dart dominando assincronismo e criando comunicação com APIs/dart_assincronismo/bin/main.dart
+++ Dart dominando assincronismo e criando comunicação com APIs/dart_assincronismo/bin/main.dart
@@ -6,8 +6,7 @@ void main() {
 }
 
 void requestData() {
-requestData() {
-  String url = "https://gist.githubusercontent.com/NatashaB-randao/674b50f61abe7496a9c2c88f81d7b8f5/raw/5d04a2193ea1c0248a698a4a687360a309482dcb/accounts.json";
-  print(get(Uri.parse(url)));
-
+  String url = "https://gist.githubusercontent.com/ricarthlima/a0eb198cb7a70696c4031e7e577de0cd/raw/356ce2c39dfd58d3d2e948d1ad87ea828544f1db/accounts.json";
+  Future<Response> futureResponse = get(Uri.parse(url));
+  print(futureResponse);
 }
