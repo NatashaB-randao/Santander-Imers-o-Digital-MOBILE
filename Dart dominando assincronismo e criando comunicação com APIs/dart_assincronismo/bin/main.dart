705c2a04f4e3bb6e229d5c84f3a57ed572925761
Natasha Brandão <79339286+NatashaB-randao@users.noreply.github.com>
2025-12-04 12:46:55 -0300

    Add files via upload

---
+++ Dart dominando assincronismo e criando comunicação com APIs/dart_assincronismo/bin/main.dart
@@ -0,0 +1,12 @@
+import 'package:http/http.dart';
+
+void main() {
+  //print('Hello, World!');
+  requestData();
+}
+
+requestData() {
+  String url = "https://gist.githubusercontent.com/NatashaB-randao/674b50f61abe7496a9c2c88f81d7b8f5/raw/5d04a2193ea1c0248a698a4a687360a309482dcb/accounts.json";
+  print(get(Uri.parse(url)));
+
+}
