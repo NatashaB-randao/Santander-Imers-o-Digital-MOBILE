6f2929dfd89d57f01fd34e7bac7bca0a0ecd9fa5
NatashaB-randao <natashabrandao57@hotmail.com>
2025-12-04 14:08:57 -0300

    Entendendo o Future

--- Dart dominando assincronismo e criando comunicação com APIs/dart_assincronismo/bin/main.dart
+++ Dart dominando assincronismo e criando comunicação com APIs/dart_assincronismo/bin/main.dart
@@ -9,4 +9,9 @@ void requestData() {
   String url = "https://gist.githubusercontent.com/ricarthlima/a0eb198cb7a70696c4031e7e577de0cd/raw/356ce2c39dfd58d3d2e948d1ad87ea828544f1db/accounts.json";
   Future<Response> futureResponse = get(Uri.parse(url));
   print(futureResponse);
+  futureResponse.then((Response response) {
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+    
+  },);
 }
